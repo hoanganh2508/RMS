20411c538ae4040e1aa17450181e18b5ce45a2fa
hoang anh <hoanganhle30081995@gmail.com>
2022-03-16 18:36:05 +0700

    fix table_controller

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,12 +1,5 @@
 class TablesController < ApplicationController
-  before_action :find_table, only: [:index]
-
   def index
-    @table = Table.all
+    @tables = Table.all
   end
-
-  private
-    def find_table
-      @table = Table.find_by(id: params[:id])
-    end
 end
